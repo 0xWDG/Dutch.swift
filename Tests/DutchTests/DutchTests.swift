d161f510ffc97ec8e1f077989c1fc238185db82c
Wesley de Groot <email@wesleydegroot.nl>
2024-10-04 13:44:56 +0200

    Fix lint errors

--- Tests/DutchTests/DutchTests.swift
+++ Tests/DutchTests/DutchTests.swift
@@ -8,9 +8,3 @@
 //  https://github.com/0xWDG/Dutch
 //  MIT License
 //
-import Testing
-@testable import Dutch
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
